23ab446675c09f816e79f0cbcdc651e93ea3e6c5
DiyaS-77 <DIYA.S@CAPGEMINI.COM>
2026-02-13 12:26:30 +0530

    Update gap_handle.py

--- gap_handle.py
+++ gap_handle.py
@@ -1,35 +1,69 @@
-2026-02-13 12:11:01,920 | INFO |  ===== [5/79] Running GAP/BROB/BCST/BV-01-C =====
-2026-02-13 12:11:38,730 | INFO | Running test case: GAP/BROB/BCST/BV-01-C
-2026-02-13 12:11:38,730 | INFO | Running test case: GAP / GAP/BROB/BCST/BV-01-C
-2026-02-13 12:11:38,731 | INFO | MMI RECEIVED
-2026-02-13 12:11:38,732 | INFO | Project   : GAP
-2026-02-13 12:11:38,732 | INFO | WID       : 47
-2026-02-13 12:11:38,733 | INFO | Testcase  : GAP/BROB/BCST/BV-01-C
-2026-02-13 12:11:38,733 | INFO | Desc      : Please enter broadcasting mode using specified TSPX_advertising_data value and start advertising.
-2026-02-13 12:11:38,734 | INFO | Style     : MMI_Style_Ok_Cancel2
-2026-02-13 12:11:38,734 | INFO | Resolving handler: handle_wid_47
-2026-02-13 12:11:38,734 | INFO | Executing GAP handler for WID 47
-2026-02-13 12:11:38,735 | INFO | [gRPC] start_advertising failed: Either advertisement_data_key or advertisement_structure_key must be provided
-2026-02-13 12:11:38,735 | INFO | MMI handling failed: 'NoneType' object is not callable
-2026-02-13 12:11:38,735 | INFO | Server: SUCCESS with response=OK
-2026-02-13 12:11:38,735 | INFO | GAP/BROB/BCST/BV-01-C
-2026-02-13 12:11:38,736 | INFO | INDCSV
-2026-02-13 12:11:38,736 | INFO | ---- PTS FINAL VERDICT ----
-2026-02-13 12:11:38,736 | INFO | RESULT: INDCSV
-2026-02-13 12:11:38,736 | INFO | Resetting DUT between tests
-2026-02-13 12:11:38,736 | INFO | PTS test state stopped
-2026-02-13 12:11:38,736 | INFO | GAP/BROB/BCST/BV-01-C DONE
-2026-02-13 12:11:38,737 | INFO | 
-2026-02-13 12:11:38,737 | INFO | ==============================
-2026-02-13 12:11:38,737 | INFO | PTS EXECUTION SUMMARY
-2026-02-13 12:11:38,737 | INFO | ==============================
-2026-02-13 12:11:38,738 | INFO | GAP/BROB/OBSV/BV-01-C : PASS
-2026-02-13 12:11:38,738 | INFO | GAP/BROB/OBSV/BV-02-C : PASS
-2026-02-13 12:11:38,738 | INFO | GAP/BROB/OBSV/BV-05-C : PASS
-2026-02-13 12:11:38,738 | INFO | GAP/BROB/OBSV/BV-06-C : INDCSV
-2026-02-13 12:11:38,738 | INFO | GAP/BROB/BCST/BV-01-C : INDCSV
-2026-02-13 12:11:38,739 | INFO | ------------------------------
-2026-02-13 12:11:38,739 | INFO | Total   : 5
-2026-02-13 12:11:38,739 | INFO | Passed  : 3
-2026-02-13 12:11:38,739 | INFO | Failed  : 0
-2026-02-13 12:11:38,739 | INFO | ==============================
+2026-02-13 12:25:32,653 | INFO | ==============================
+2026-02-13 12:25:32,653 | INFO | gRPC Client Started
+2026-02-13 12:25:32,653 | INFO | ==============================
+2026-02-13 12:25:32,657 | INFO | Triggering PTS PROJECT via gRPC
+2026-02-13 12:25:32,657 | INFO | Loading testbed configuration from: config.json
+2026-02-13 12:25:32,669 | INFO | Loaded 79 testcases for GAP
+2026-02-13 12:25:32,669 | INFO |  ===== [1/79] Running GAP/BROB/OBSV/BV-01-C =====
+2026-02-13 12:25:53,592 | INFO | Starting project execution: GAP
+2026-02-13 12:25:53,592 | INFO | Loading testbed configuration from: config.json
+2026-02-13 12:25:53,593 | INFO | Waiting for gRPC server startup 10.91.220.35
+2026-02-13 12:25:53,593 | INFO | gRPC server is running 10.91.220.35
+2026-02-13 12:25:53,594 | INFO | Opening PTS workspace once
+2026-02-13 12:25:53,594 | INFO | Opening existing workspace: C:\PTS\Workspaces\GAP_WS\GAP_WS.pqw6
+2026-02-13 12:25:53,595 | INFO | Selecting PTS project once
+2026-02-13 12:25:53,595 | INFO | Selecting project: GAP
+2026-02-13 12:25:53,595 | INFO | Running test case: GAP/BROB/OBSV/BV-01-C
+2026-02-13 12:25:53,596 | INFO | Registering PTS logger
+2026-02-13 12:25:53,596 | INFO | Registering MMI handler
+2026-02-13 12:25:53,596 | INFO | Running test case: GAP / GAP/BROB/OBSV/BV-01-C
+2026-02-13 12:25:53,596 | INFO | MMI RECEIVED
+2026-02-13 12:25:53,597 | INFO | Project   : GAP
+2026-02-13 12:25:53,597 | INFO | WID       : 12
+2026-02-13 12:25:53,597 | INFO | Testcase  : GAP/BROB/OBSV/BV-01-C
+2026-02-13 12:25:53,598 | INFO | Desc      : Please start passive scanning. Press OK to continue.
+2026-02-13 12:25:53,598 | INFO | Style     : MMI_Style_Ok_Cancel1
+2026-02-13 12:25:53,599 | INFO | Resolving handler: handle_wid_12
+2026-02-13 12:25:53,599 | INFO | Executing GAP handler for WID 12
+2026-02-13 12:25:53,600 | INFO | WID 12 completed with result: True
+2026-02-13 12:25:53,600 | INFO | MMI response: OK
+2026-02-13 12:25:53,600 | INFO | Server: SUCCESS with response=OK
+2026-02-13 12:25:53,600 | INFO | MMI RECEIVED
+2026-02-13 12:25:53,600 | INFO | Project   : GAP
+2026-02-13 12:25:53,601 | INFO | WID       : 4
+2026-02-13 12:25:53,601 | INFO | Testcase  : GAP/BROB/OBSV/BV-01-C
+2026-02-13 12:25:53,601 | INFO | Desc      : Please confirm that IUT has received an Advertising Event then click OK.
+2026-02-13 12:25:53,601 | INFO | Style     : MMI_Style_Ok_Cancel1
+2026-02-13 12:25:53,601 | INFO | MMI response: OK
+2026-02-13 12:25:53,601 | INFO | GAP/BROB/OBSV/BV-01-C
+2026-02-13 12:25:53,601 | INFO | PASS
+2026-02-13 12:25:53,601 | INFO | ---- PTS FINAL VERDICT ----
+2026-02-13 12:25:53,602 | INFO | RESULT: PASS
+2026-02-13 12:25:53,602 | INFO | Resetting DUT between tests
+2026-02-13 12:25:53,602 | INFO | PTS test state stopped
+2026-02-13 12:25:53,602 | INFO | GAP/BROB/OBSV/BV-01-C DONE
+2026-02-13 12:25:53,602 | INFO |  ===== [2/79] Running GAP/BROB/OBSV/BV-02-C =====
+2026-02-13 12:26:01,213 | INFO | Running test case: GAP/BROB/OBSV/BV-02-C
+2026-02-13 12:26:01,214 | INFO | Running test case: GAP / GAP/BROB/OBSV/BV-02-C
+2026-02-13 12:26:01,214 | INFO | MMI RECEIVED
+2026-02-13 12:26:01,215 | INFO | Project   : GAP
+2026-02-13 12:26:01,215 | INFO | WID       : 169
+2026-02-13 12:26:01,215 | INFO | Testcase  : GAP/BROB/OBSV/BV-02-C
+2026-02-13 12:26:01,216 | INFO | Desc      : Please start observation procedure. Press OK to continue.
+2026-02-13 12:26:01,216 | INFO | Style     : MMI_Style_Ok_Cancel1
+2026-02-13 12:26:01,217 | INFO | MMI response: OK
+2026-02-13 12:26:01,217 | INFO | Server: SUCCESS with response=OK
+2026-02-13 12:26:01,217 | INFO | MMI RECEIVED
+2026-02-13 12:26:01,217 | INFO | Project   : GAP
+2026-02-13 12:26:01,218 | INFO | WID       : 4
+2026-02-13 12:26:01,218 | INFO | Testcase  : GAP/BROB/OBSV/BV-02-C
+2026-02-13 12:26:01,218 | INFO | Desc      : Please confirm that IUT has received an Advertising Event then click OK.
+2026-02-13 12:26:01,219 | INFO | Style     : MMI_Style_Ok_Cancel1
+2026-02-13 12:26:01,219 | INFO | MMI response: OK
+2026-02-13 12:26:01,219 | INFO | GAP/BROB/OBSV/BV-02-C
+2026-02-13 12:26:01,220 | INFO | PASS
+2026-02-13 12:26:01,220 | INFO | ---- PTS FINAL VERDICT ----
+2026-02-13 12:26:01,220 | INFO | RESULT: PASS
+2026-02-13 12:26:01,220 | INFO | Resetting DUT between tests
+2026-02-13 12:26:01,221 | INFO | PTS test state stopped
+2026-02-13 12:26:01,221 | INFO | GAP/BROB/OBSV/BV-02-C DONE
